194302200a191d6aac9f6fd4d5c3cb2ded0e7998
tnaoki0630 <naoki.tsunezawa.s8@dc.tohoku.ac.jp>
2025-06-29 18:33:22 +0900

    Update README.md

--- tools/parseXML.py
+++ tools/parseXML.py
@@ -40,14 +40,14 @@ def parseXML(xml_file, section_name, tag_name):
 
     return cycle_ids, values
 
-def plot_values(cycles, arr_values, labels, title="", scale=1.0):
+def plot_values(cycles, arr_values, labels, scales, title="", xscale=1.0):
     plt.figure()
-    for values, label_values in zip(arr_values, labels):
-        plt.plot(cycles, [val*scale for val in values], label=label_values)
+    for values, label_values, scale in zip(arr_values, labels, scales):
+        plt.plot([cyc*xscale for cyc in cycles], [val*scale for val in values], label=label_values)
     plt.legend(loc='lower right')
     plt.margins(x=0.0, y=0.30)
     plt.ylabel(title)
-    plt.xlabel('Cycle')
+    plt.xlabel('time [ns]')
     # plt.xlim(left=0)
     plt.ylim(bottom=0)
     plt.tight_layout()
@@ -63,6 +63,9 @@ if __name__ == '__main__':
     cycles, Ni_large2 = parseXML('large2_20250623042317_log.xml', 'flowout_ion_Xe1', 'particleNumber')
     cycles, Ne_large3 = parseXML('large3_20250624212336_log.xml', 'flowout_electron', 'particleNumber')
     cycles, Ni_large3 = parseXML('large3_20250624212336_log.xml', 'flowout_ion_Xe1', 'particleNumber')
-    values = [Ne_long[0:cycle], Ni_long[0:cycle], Ne_large[0:cycle], Ni_large[0:cycle], Ne_large2[0:cycle], Ni_large2[0:cycle], Ne_large3[0:cycle], Ni_large3[0:cycle]]
-    labels = ["Ne_ippc10", "Ni_ippc10", "Ne_ippc20", "Ni_ippc20", "Ne_ippc30", "Ni_ippc30", "Ne_ippc50", "Ni_ippc50"]
-    plot_values(cycles[0:cycle], values, labels, "particle Number [-]", 1)
+    cycles, Ne_large4 = parseXML('large4_20250626124908_log.xml', 'flowout_electron', 'particleNumber')
+    cycles, Ni_large4 = parseXML('large4_20250626124908_log.xml', 'flowout_ion_Xe1', 'particleNumber')
+    values = [Ne_long[0:cycle], Ni_long[0:cycle], Ne_large[0:cycle], Ni_large[0:cycle], Ne_large2[0:cycle], Ni_large2[0:cycle], Ne_large3[0:cycle], Ni_large3[0:cycle], Ne_large4[0:cycle], Ni_large4[0:cycle]]
+    labels = ["Ne_ippc10", "Ni_ippc10", "Ne_ippc20", "Ni_ippc20", "Ne_ippc30", "Ni_ippc30", "Ne_ippc50", "Ni_ippc50", "Ne_ippc100(ngy=50)", "Ni_ippc100(ngy=50)"]
+    scales = [12.5e4, 12.5e4, 6.25e4, 6.25e4, 4.166667e4, 4.166667e4, 2.5e4, 2.5e4, 1.25e4*4, 1.25e4*4]
+    plot_values(cycles[0:cycle], values, labels, scales, "particle Number [1/cm]", 5e-3)
